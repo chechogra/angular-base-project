2256d0e3e133754d6075c07c1f7473001180fbd3
Sergio Granada <chechogra@gmail.com>
2016-05-31 13:12:37 -0500

    Angular translate setup

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,7 +6,7 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig) {
+  function config($translateProvider, $logProvider, toastrConfig) {
     // Enable log
     $logProvider.debugEnabled(true);
 
@@ -16,6 +16,30 @@
     toastrConfig.positionClass = 'toast-top-right';
     toastrConfig.preventDuplicates = true;
     toastrConfig.progressBar = true;
+
+    //Angular translate configuration
+    $translateProvider.preferredLanguage('en');
+
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'app/lang/',
+      suffix: '.json'
+    });
+
+    $translateProvider.registerAvailableLanguageKeys(['en', 'es'], {
+      'en_SG': 'en',
+      'en_UK': 'en',
+      'en_US': 'en',
+      'es_145': 'es',
+      'es_ES': 'es'
+    });
+
+    $translateProvider.useLocalStorage();
+
+    $translateProvider.determinePreferredLanguage(function () {
+      return 'en';
+    }).fallbackLanguage('en');
+
+    $translateProvider.useSanitizeValueStrategy('escape');
   }
 
 })();
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('angularBaseProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('angularBaseProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr', 'pascalprecht.translate']);
 
 })();
