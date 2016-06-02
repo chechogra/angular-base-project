26ad2dbdb1530951d32af3f194db616364b34067
Sergio Granada <chechogra@gmail.com>
2016-06-02 09:39:31 -0500

    search event setup

--- src/app/components/search-box/search-box.directive.js
+++ src/app/components/search-box/search-box.directive.js
@@ -6,14 +6,21 @@
     .directive('searchBox', searchBox);
 
   /** @ngInject */
-  function searchBox() {
+  function searchBox($rootScope) {
     return{
       restrict: 'E',
       scope: {},
       replace: true,
       templateUrl: 'app/components/search-box/search-box.html',
-      link: function ($scope) {
-        $scope.sample = '';
+      link: function (scope) {
+        scope.fireSearch = function (keyEvent) {
+          if(keyEvent.which === 13){
+            var searchValue = keyEvent.target.value;
+            if(searchValue){
+              $rootScope.$broadcast('searchEvent', searchValue);
+            }
+          }
+        };
       }
     }
   }
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,18 +6,25 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($rootScope, $http, vimeoConfig) {
+  function runBlock($rootScope, $http, $state, vimeoConfig) {
 
     $http.defaults.headers.common.Authorization = 'Bearer ' + vimeoConfig.ACCESS_TOKEN;
 
-    var deregisterationstateChangeError = $rootScope.$on('$stateChangeError',
+    var deregisterationStateChangeError = $rootScope.$on('$stateChangeError',
       function (event, toState, toParams, fromState, fromParams, error) {
         if (error) {
           event.preventDefault();
         }
       });
 
-    $rootScope.$on('$destroy', deregisterationstateChangeError);
+    var deregisterationSearchEvent = $rootScope.$on('searchEvent', function (event, data) {
+      $state.go('main.search', { query: data });
+    });
+
+    $rootScope.$on('$destroy', function () {
+      deregisterationStateChangeError();
+      deregisterationSearchEvent();
+    });
   }
 
 })();
