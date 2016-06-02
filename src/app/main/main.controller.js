3ef0ccca798c0e6ef60ad3783f1a1b10336e5af3
Sergio Granada <chechogra@gmail.com>
2016-06-02 00:09:08 -0500

    transitions between page changes

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,13 +6,18 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($stateParams, sidenavLinks, responseVideos, vimeoConfig, $log) {
+  function MainController($state, $stateParams, sidenavLinks, responseVideos, vimeoConfig, $log) {
     var vm = this;
     vm.sidenavLinks = sidenavLinks;
     vm.totalItems = (responseVideos && responseVideos.data) ? responseVideos.data.total : 0;
     vm.page = $stateParams.page;
     vm.maxPaginatorSize = vimeoConfig.MAX_PAGINATOR_SIZE;
     vm.itemsPerPage = vimeoConfig.PER_PAGE;
+
+    vm.pageChanged = function () {
+      $state.go('.', { page: vm.page});
+    };
+
     $log.info(responseVideos);
   }
 })();
