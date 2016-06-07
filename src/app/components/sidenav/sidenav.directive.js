cf8fe9405e20f46e4cc6da78063a8ef22d4447f9
Sergio Granada <chechogra@gmail.com>
2016-06-07 13:40:39 -0500

    sidenav buttons implementation

--- src/app/components/sidenav/sidenav.directive.js
+++ src/app/components/sidenav/sidenav.directive.js
@@ -6,14 +6,57 @@
     .directive('sidenav', sidenavDirective);
 
   /** @ngInject */
-  function sidenavDirective() {
+  function sidenavDirective($document) {
     return{
       restrict: 'E',
       scope: {
         items: '='
       },
       replace: true,
-      templateUrl: 'app/components/sidenav/sidenav.html'
+      templateUrl: 'app/components/sidenav/sidenav.html',
+      link: function(scope, element){
+        var toogleButtonDOM = $document[0].querySelector('.toogle-sidenav');
+        var closeButtonDOM = $document[0].querySelector('.close-sidenav');
+        var toogleButton, closeButton;
+
+        var toogleSidenav = function () {
+          if(element.hasClass('open')){
+            element.removeClass('open');
+          }else{
+            element.addClass('open');
+          }
+        };
+
+        if(toogleButtonDOM){
+          toogleButton = angular.element(toogleButtonDOM);
+          toogleButton.on('click', toogleSidenav);
+
+        }
+
+        var closeSidenav = function () {
+          if(element.hasClass('open')){
+            element.removeClass('open');
+          }
+        };
+
+        if(closeButtonDOM){
+          closeButton = angular.element(closeButtonDOM);
+          closeButton.on('click', closeSidenav);
+
+        }
+
+        scope.$on('$destroy', function () {
+          if(toogleButton){
+            toogleButton.off('click', toogleSidenav);
+          }
+
+          if(closeButton){
+            closeButton.off('click', closeSidenav);
+          }
+
+        });
+
+      }
     }
   }
 })();
